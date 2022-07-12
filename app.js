0a92c747cc5bb2a7017c83c62640efdc887d92b9
dmlfahfn <dmlfahfn1@hotmail.se>
2022-07-13 00:14:25 +0200

    something

--- app.js
+++ app.js
@@ -10,14 +10,11 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(cors());
-// app.use(express.static('public')); 
-// app.use('/images', express.static('images'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/public/images',express.static(__dirname + '/public/images'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
